47cef7d2ac85351a11760f16613d6eb36d353a7c
vamshi mamidala <vamshimamidala12@gmail.com>
2024-10-20 13:06:53 +0530

    landing page commit

---
+++ dbcoonection/dbconne.js
@@ -0,0 +1,2 @@
+let mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://vamshimamidala12:Zj5YhVURDkSjDXjn@landingpage.lp3b2.mongodb.net/?retryWrites=true&w=majority&appName=landingpage");
